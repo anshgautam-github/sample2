044cacefc30a7682e728630b9d992e46944b2d26
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-12-23 19:24:56 +0530

    Update filename.java

--- sample/filename.java
+++ sample/filename.java
@@ -1,1 +1,16 @@
 
+
+//single line comment
+
+/*
+
+    this is a muliline comment
+    it follow multiline behaviour
+*/
+
+
+class filename {
+    public static void main(String[] args) {
+        System.out.println("Sample code");
+    }
+}
